487968f787b7674b002a69e33e19570e23afd5ac
MinseoKangQ <kms02171@naver.com>
2024-03-18 19:07:02 +0900

    :tada: 프로젝트 시작

---
+++ src/main/kotlin/com/capston/sumnote/SumNoteApplication.kt
@@ -0,0 +1,11 @@
+package com.capston.sumnote
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SumNoteApplication
+
+fun main(args: Array<String>) {
+	runApplication<SumNoteApplication>(*args)
+}

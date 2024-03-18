a0e4f67567559cf25634b4c1a77016ac2f651f69
MinseoKangQ <kms02171@naver.com>
2024-03-18 19:50:01 +0900

    :sparkles: 공통 응답 클래스 추가

---
+++ src/main/kotlin/com/capston/sumnote/util/response/CustomApiResponse.kt
@@ -0,0 +1,17 @@
+package com.capston.sumnote.util.response
+
+class CustomApiResponse<T> private constructor(
+    val status: Int,
+    val data: T?,
+    val message: String?
+) {
+    companion object {
+        fun <T> createSuccess(status: Int, data: T, message: String): CustomApiResponse<T> {
+            return CustomApiResponse(status, data, message)
+        }
+
+        fun <T> createFailWithoutData(status: Int, message: String): CustomApiResponse<T> {
+            return CustomApiResponse(status, null, message)
+        }
+    }
+}

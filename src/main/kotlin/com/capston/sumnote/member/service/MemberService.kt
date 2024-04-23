4020377a7f46bb98d54cb06db5ecb5817079ccf9
MinseoKangQ <kms02171@naver.com>
2024-04-24 01:07:12 +0900

    :sparkles: 노트 생성/조회 API 작성

--- src/main/kotlin/com/capston/sumnote/member/service/MemberService.kt
+++
@@ -1,9 +0,0 @@
-package com.capston.sumnote.member.service
-
-import com.capston.sumnote.member.dto.LoginDto
-import com.capston.sumnote.util.response.CustomApiResponse
-
-interface MemberService {
-    fun login(dto: LoginDto.Req): Pair<LoginDto.Res, String>
-    fun withdraw(email: String): CustomApiResponse<*>
-}
